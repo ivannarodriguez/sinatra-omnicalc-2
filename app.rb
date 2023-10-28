62d70d1e5bfc7f95d672da9dcd64349c563adad1
Ivanna <44304361+ivannarodriguez@users.noreply.github.com>
2023-10-28 03:10:52 +0000

    almos

--- app.rb
+++ app.rb
@@ -5,30 +5,31 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
   @first_num = params.fetch("first_num").to_f
-
+  @second_num = params.fetch("second_num").to_f
   @result = @first_num + @second_num
   erb(:add_result)
 end
 
 get("/subtract") do
-  erb(:sub_form)
+  erb(:subtraction_form)
 end
 
 get("/wizard_subtract") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
+  @result = @second_num - @first_num
 
   erb(:sub_result)
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
 get("/wizard multiply") do
@@ -36,9 +37,12 @@ get("/wizard multiply") do
 end
 
 get("/divide") do
-  erb(:div_form)
+  erb(:divform)
 end
 
 get("/wizard_divide") do
+  @first = params.fetch("first_number").to_f
+  @second = params.fetch("second_num").to_f
+  @result = @first/@second
   erb(:div_result)
 end
